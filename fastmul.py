fb67c9369242d808e1bc2cff1359b43468c125fa
vladimirkokshenev <vladimirkokshenev@gmail.com>
2017-09-13 10:23:40 -0700

    complete implementation for recoursive multiplication

--- fastmul.py
+++ fastmul.py
@@ -12,9 +12,10 @@ def convert_string_to_big_integer(s):
         arg.append(int(s[i]))
     return arg
 
+
 def split_big_integer(arg):
     """
-    Take big intger in form of a list, and splits it into to integers of size len(arg)/2
+    Take big integer in form of a list, and splits it into to integers of size len(arg)/2
     
     :param arg: list of integers
     :return: a,b pair containing two lists of integers. a.append(b) is arg. 
@@ -30,6 +31,7 @@ def split_big_integer(arg):
         b.append(arg[i])
 
     return a, b
+
 
 def sum_big_integers(a, b):
     """
@@ -69,6 +71,71 @@ def sum_big_integers(a, b):
     return sum
 
 
+def shift_big_integer(a, n):
+    """ Shift big integer a for n digit left
+    
+    :param a: big integer as a list
+    :param n: integer
+    :return: 
+    """
+
+    for i in range(n):
+        a.append(0)
+
+    return a
+
+
+def mul_big_integers(x, y):
+    """ Multiplies two big integers and returns their product
+    
+    :param x: 1st argument 
+    :param y: 2nd argument
+    :return: product
+    """
+
+    mul = []
+
+    if len(x) == 1:
+        # multiply every digit of y by x, going from lower to upper digits
+        for i in range(len(y)-1, -1, -1):
+            mul.insert(0,x[0]*y[i])
+        # resolve overflow
+        for i in range(len(mul) - 1, 0, -1):
+            mul[i-1] += mul[i] / 10
+            mul[i] = mul[i] % 10
+        if mul[0] > 9:
+            cr = mul[0] / 10
+            mul[0] = mul[0] % 10
+            mul.insert(0, cr)
+        return mul
+
+    if len(y) == 1:
+        return mul_big_integers(y, x)
+
+    # x = a*10^n1 + b
+    a, b = split_big_integer(x)
+    n1 = len(b)
+    # y = c*10^n2 + d
+    c, d = split_big_integer(y)
+    n2 = len(d)
+
+    ac = mul_big_integers(a, c)
+    ad = mul_big_integers(a, d)
+    bc = mul_big_integers(b, c)
+    bd = mul_big_integers(b, d)
+
+    res1 = shift_big_integer(ac, n1+n2)
+    res2 = shift_big_integer(ad, n1)
+    res3 = shift_big_integer(bc, n2)
+
+    res4 = sum_big_integers(res1, res2)
+    res5 = sum_big_integers(res4, res3)
+
+    res = sum_big_integers(res5, bd)
+
+    return res
+
+
 if __name__ == "__main__":
 
     successful_input = False
@@ -93,3 +160,13 @@ if __name__ == "__main__":
 
     print(arg1)
     print(arg2)
+
+    res = mul_big_integers(arg1, arg2)
+
+    s = ""
+
+    for i in range(len(res)):
+        s += str(res[i])
+
+    print("Result:")
+    print(s)
--- tests/testfastmul.py
+++ tests/testfastmul.py
@@ -45,40 +45,112 @@ class TestSplit(unittest.TestCase):
 class TestSum(unittest.TestCase):
     """Unit tests for sum_big_integers()    """
 
-    def test_len1(self):
+    def test_sum_len1(self):
         """ Testing sum_big_integers() with 2 single digit integers without overflow"""
         a = [1]
         b = [2]
         sum = fastmul.sum_big_integers(a, b)
         self.assertEqual(sum, [3])
 
-    def test_len1overflow(self):
+    def test_sum_len1overflow(self):
         """ Testing sum_big_integers() with 2 single digit integers with overflow"""
         a = [5]
         b = [6]
         sum = fastmul.sum_big_integers(a, b)
         self.assertEqual(sum, [1, 1])
 
-    def test_len2(self):
+    def test_sum_len2(self):
         """ Testing sum_big_integers() with 2 double digit integers without overflow"""
         a = [1, 1]
         b = [2, 2]
         sum = fastmul.sum_big_integers(a, b)
         self.assertEqual(sum, [3, 3])
 
-    def test_len2overflow(self):
+    def test_sum_len2overflow(self):
         """ Testing sum_big_integers() with 2 double digit integers with multiple overflow"""
         a = [9, 9]
         b = [9, 9]
         sum = fastmul.sum_big_integers(a, b)
         self.assertEqual(sum, [1, 9, 8])
 
-    def test_lenmisc(self):
+    def test_sum_lenmisc(self):
         """ Testing sum_big_integers() with 2 integers of different size with overflow"""
         a = [9, 9]
         b = [2, 9, 9, 9]
         sum = fastmul.sum_big_integers(a, b)
         self.assertEqual(sum, [3, 0, 9, 8])
+
+
+class TestShift(unittest.TestCase):
+    """Unit Tests for shifting big integers"""
+
+    def test_shift_a_n(self):
+        """ Test shifting of 3 digit integer for 3 positions"""
+        a = [1, 2, 3]
+        n = 3
+        self.assertEqual(fastmul.shift_big_integer(a, n), [1, 2, 3, 0, 0, 0])
+
+
+class TestMul(unittest.TestCase):
+    """Unit tests for multiplication"""
+
+    def test_mul_x1(self):
+        """ Testing mul_big_integers(x, y) with single digit x and without overflow"""
+        x = [2]
+        y = [2, 2]
+        mul = fastmul.mul_big_integers(x, y)
+        self.assertEqual(mul, [4, 4])
+
+    def test_mul_x1_overflow(self):
+        """ Testing mul_big_integers(x, y) with single digit x and with overflow"""
+        x = [9]
+        y = [9, 9, 9]
+        mul = fastmul.mul_big_integers(x, y)
+        self.assertEqual(mul, [8, 9, 9, 1])
+
+    def test_mul_y1(self):
+        """ Testing mul_big_integers(x, y) with single digit y and without overflow"""
+        x = [2, 2]
+        y = [2]
+        mul = fastmul.mul_big_integers(x, y)
+        self.assertEqual(mul, [4, 4])
+
+    def test_mul_y1_overflow(self):
+        """ Testing mul_big_integers(x, y) with single digit y and with overflow"""
+        x = [9, 9, 9]
+        y = [9]
+        mul = fastmul.mul_big_integers(x, y)
+        self.assertEqual(mul, [8, 9, 9, 1])
+
+    def test_mul_x2_y2(self):
+        """Testing multiplication of 2-digit numbers"""
+        x = [3, 7]
+        y = [7, 3]
+        self.assertEqual(fastmul.mul_big_integers(x, y), [2, 7, 0, 1])
+
+    def test_mul_x4_y4(self):
+        """Testing multiplication of 4-digit numbers"""
+        x = [1, 2, 3, 4]
+        y = [5, 6, 7, 8]
+        self.assertEqual(fastmul.mul_big_integers(x, y), [7, 0, 0, 6, 6, 5, 2])
+
+    def test_mul_x6_y3(self):
+        """Testing multiplication of 6-digit and 3-digit numbers"""
+        x = [1, 2, 3, 4, 5, 6]
+        y = [9, 8, 7]
+        self.assertEqual(fastmul.mul_big_integers(x, y), [1, 2, 1, 8, 5, 1, 0, 7, 2])
+
+    def test_mul_real_big(self):
+        """Test multiplication with really big numbers"""
+        a = 2718281828459045235360287471352662497757247093699959574966967627
+        b = 3141592653589793238462643383279502884197169399375105820974944592
+        a_str = str(a)
+        b_str = str(b)
+        x = fastmul.convert_string_to_big_integer(a_str)
+        y = fastmul.convert_string_to_big_integer(b_str)
+        res = fastmul.convert_string_to_big_integer(str(a*b))
+        self.assertEqual(fastmul.mul_big_integers(x, y), res)
+
 
 if __name__ == '__main__':
     unittest.main()
