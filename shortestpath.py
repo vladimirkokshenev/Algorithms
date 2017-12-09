c64bd1f26c374e5e6af83cc2c15d2576146ce370
VK <vladimirkokshenev@gmail.com>
2017-12-08 22:41:21 -0800

    shortest path implementations for dijkstra+minheap and Johnson's algs

--- shortestpath.py
+++ shortestpath.py
@@ -1,4 +1,6 @@
 import readgraph
+from mst import MinHeap
+from copy import deepcopy
 
 
 def bellman_ford(G, src):
@@ -61,11 +63,152 @@ def floyd_warshall(G):
     return A_prev
 
 
+def dijkstra(G, s):
+    """
+    Computes shortest paths on graph G from node i to all other nodes.
+    :param G: graph as a adjacency matrix
+    :param i: source node
+    :return: list of shortest paths
+    """
+
+    n = G[0]["N"]
+
+    shortest_path = [readgraph.INF for i in range(n+1)]
+    shortest_path[s] = 0
+
+    min_heap_init = list(shortest_path)
+
+    for d in G[s]["OUT"]:
+        min_heap_init[d] = G[s]["OUT"][d]
+
+    mheap = MinHeap(min_heap_init)
+
+    visited_nodes = set([s])
+
+    while len(visited_nodes) < n:
+        # choose an edge using dijkstra greedy score
+
+        next_node = mheap.pop()
+        visited_nodes.add(next_node["index"])
+        shortest_path[next_node["index"]] = next_node["key"]
+
+        for d in G[next_node["index"]]["OUT"]:
+            mheap.change_key(d, next_node["key"] + G[next_node["index"]]["OUT"][d])
+
+    return shortest_path
+
+
+def johnson_1_extra_node(G):
+    """
+    Implementaion of step 1 of Johnson's algorithm. It adds new node <n+1> to graph G.
+    Node <n+1> has connections to all other nodes, with the cost of 0.
+    :param G: input graph G as in/out adjacency list
+    :return: new graph Gm
+    """
+
+    Gm = deepcopy(G)
+    Gm[0]["E"] += Gm[0]["N"]
+    Gm[0]["N"] += 1
+    Gm.append({"IN":{}, "OUT":{}})
+    for i in range(1, Gm[0]["N"]):
+        Gm[i]["IN"][Gm[0]["N"]] = 0
+        Gm[Gm[0]["N"]]["OUT"][i] = 0
+
+    return Gm
+
+
+def johnson_3_adjust_edge_costs(G, bfsp):
+    """
+    Step 3 of Johnson's algorithm. It uses shortest paths from extra node to all other nodes, obtained on step 2,
+    to adjust edge costs (u,v) as Ce_new = Ce + bfsp[u] - bfsp[v]
+    :param G: input graph G as in/out adjacency list
+    :param bfsp: bellman-ford shortest paths from new node to all existing nodes
+    :return: new graph Gm with updated edge costs
+    """
+
+    Gm = deepcopy(G)
+    n = Gm[0]["N"]
+
+    for u in range(1, n+1):
+        for v in Gm[u]["OUT"]:
+            new_edge_cost = Gm[u]["OUT"][v] + bfsp[-1][u] - bfsp[-1][v]
+            Gm[u]["OUT"][v] = new_edge_cost
+            Gm[v]["IN"][u] = new_edge_cost
+
+    return Gm
+
+
+def johnson_5_adjust_shortest_paths_values(ssp, bfsp):
+    """
+    Step 5 of Johnson's algorithm. It adjusts back shortest path values ssp obtained by dijkstra algorithm
+    on step 4 back. ssp[u][v] = ssp[u][v] - bfsp[u] + bfsp[v]
+    :param ssp: shortest paths obtained by dijkstra
+    :param bfsp: bellman-ford computations from step 2
+    :return: modified ssp
+    """
+    n = len(ssp)
+    for u in range(1, n):
+        for v in range(1, n):
+            ssp[u][v] = ssp[u][v] - bfsp[-1][u] + bfsp[-1][v]
+
+    return ssp
+
+
+def johnson(G):
+    """
+    Implementation of Johnson's algorithm to find all shortest paths in graph G.
+    :param G: input graph G as in/out adjacency list
+    :return: list A (with all solutions obtained from dijkstra)
+    """
+
+    # step 1 - add extra node
+    Gm = johnson_1_extra_node(G)
+
+    # step 2 - run bellman-ford from this extra node
+    A = bellman_ford(Gm, Gm[0]["N"])
+    if not A:
+        print("Negative cycle detected!")
+        exit(1)
+
+    # step 3 - adjust edge costs
+    Gm = johnson_3_adjust_edge_costs(G, A)
+
+    # step 4 - run dijkstra on Gm for each node
+    ssp = []
+    ssp.append([])
+    for s in range(1, Gm[0]["N"]+1):
+        shortest_paths_from_s = dijkstra(Gm, s)
+        ssp.append(shortest_paths_from_s)
+        if s%10 == 0:
+            print("%d percents completed" % (s/10))
+
+    # step 5 - modify ssp back
+    ssp = johnson_5_adjust_shortest_paths_values(ssp, A)
+
+    return ssp
+
+
 if __name__ == '__main__':
     G = readgraph.read_graph_from_edge_list_into_in_out_adjacency_list('tests/g3.txt')
     n = G[0]["N"]
-    A = bellman_ford(G, 1)
+    ssp = johnson(G)
 
+    min_val = ssp[1][1]
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if ssp[i][j] < min_val:
+                min_val = ssp[i][j]
+
+    print("Shortest shortest path is % d" % min_val)
+
+
+
+
+
+
+
+    """
+    A = bellman_ford(G, 1)
     if not A:
         print("negative cycle detected by Belman-Ford")
     else:
@@ -78,6 +221,6 @@ if __name__ == '__main__':
                 if A[i][j] < min_val:
                     min_val = A[i][j]
         print("shortest shortest path is %d" % min_val)
-
+    """
 
 
